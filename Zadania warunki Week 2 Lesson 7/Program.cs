89189e7871228a61aa46dcaebd1786c46f6099c1
Ted K <ted.k07@gmail.com>
2023-02-09 00:18:16 +0100

    Finish exercise 4

--- Zadania warunki Week 2 Lesson 7/Program.cs
+++ Zadania warunki Week 2 Lesson 7/Program.cs
@@ -10,14 +10,12 @@ namespace Zadania_warunki_Week_2_Lesson_7
             // wersja pierwsza 
 
             Console.WriteLine("Zadanie nr 1");
-            int number1;
-            int number2;
 
             Console.WriteLine("Podaj pierwszą cyfrę ...");
-            number1 = int.Parse( Console.ReadLine());
+            int number1 = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Podaj drugą cyfrę ...");
-            number2 = int.Parse( Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
 
             if (number1 == number2)
             {
@@ -29,11 +27,30 @@ namespace Zadania_warunki_Week_2_Lesson_7
             }
 
             // wersja druga
-            
             Console.WriteLine(number1 == number2 ? $"Cyfry {number1} i {number2} są równe." : $"Cyfry {number1} i {number2} nie są równe.");
 
-            // Napisz program w C#, który sprawdzi czy podana przez użytkownika liczba jest parzysta czy nieparzysta.
+            // 2. Napisz program w C#, który sprawdzi czy podana przez użytkownika liczba jest parzysta czy nieparzysta.
 
+            Console.WriteLine("\nZadanie nr 2");
+            
+            Console.WriteLine("Sprawdz czy dana liczba jest parzysta.");
+            Console.WriteLine("Podaj liczbę ...");
+
+            int number3 = int.Parse(Console.ReadLine());
+            double result1 = number3 % 2;
+
+            Console.WriteLine(result1 == 0 ? $"{number3} jest liczbą parzysta" : $"{number3} jest liczbą NIEparzysta");
+
+            // 3. Napisz program w C#, który sprawdzi czy podana przez użytkownika liczba jest dodatnia czy ujemna.
+
+            Console.WriteLine("\nZadanie nr 3");
+            
+            Console.WriteLine("Sprawdz czy dana liczba jest dodatnia.");
+            Console.WriteLine("Podaj liczbę ...");
+
+            int number4 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine( number4 >=0 ? $"{number4} jest liczbą dodatnią" : $"{number4} jest liczbą ujemną");
 
 
 
